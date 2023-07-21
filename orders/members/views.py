83d5acf1d6ddd1cb60f4e74fc10269b5c6e8719b
Matthewous <moguch98@mail.ru>
2023-07-22 00:56:09 +0300

    модифицирована форма регистрации

--- orders/members/views.py
+++ orders/members/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 
+from .forms import RegisterUserForm
+
 # Create your views here.
 
 def login_user(request):
@@ -36,7 +38,7 @@ def register_user(request):
 
     if request.method == 'POST':
         
-        form = UserCreationForm(request.POST)
+        form = RegisterUserForm(request.POST)
         
         if form.is_valid():
             form.save()
@@ -52,8 +54,7 @@ def register_user(request):
             return redirect('home')
         
     else:
-
-        form = UserCreationForm()
+        form = RegisterUserForm()
 
 
     return render(request, 'authenticate/register_user.html', {'form':form})
